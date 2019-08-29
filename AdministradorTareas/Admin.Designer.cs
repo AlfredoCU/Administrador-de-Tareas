49e71603ffd1b9470562f1db5608454d304e1a9d
AlfredoCU <alfredo_11_cu@hotmail.com>
2019-08-28 21:38:10 -0500

    Se realizo una lista con datos fake para simular procesos de un sistema, se creo una intefaz con varias eventes que puede realizar el sistema como actualizar, finalizar proceso, los proceso se muestran en una tabla, el totaol de procesos, informacion, ayuda y salir del sistema

---
+++ AdministradorTareas/Admin.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace AdministradorTareas
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.msPrincipal = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vistaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actualizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tcProcesos = new System.Windows.Forms.TabControl();
+            this.tpProcesos = new System.Windows.Forms.TabPage();
+            this.dgvProcesos = new System.Windows.Forms.DataGridView();
+            this.tpRendimiento = new System.Windows.Forms.TabPage();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnFinalizarProceso = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtPID = new System.Windows.Forms.TextBox();
+            this.lblPID = new System.Windows.Forms.Label();
+            this.msPrincipal.SuspendLayout();
+            this.tcProcesos.SuspendLayout();
+            this.tpProcesos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProcesos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // msPrincipal
+            // 
+            this.msPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.opcionesToolStripMenuItem,
+            this.vistaToolStripMenuItem});
+            this.msPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.msPrincipal.Name = "msPrincipal";
+            this.msPrincipal.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.msPrincipal.Size = new System.Drawing.Size(1070, 24);
+            this.msPrincipal.TabIndex = 0;
+            this.msPrincipal.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.informaciónToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("archivoToolStripMenuItem.Image")));
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // informaciónToolStripMenuItem
+            // 
+            this.informaciónToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("informaciónToolStripMenuItem.Image")));
+            this.informaciónToolStripMenuItem.Name = "informaciónToolStripMenuItem";
+            this.informaciónToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.informaciónToolStripMenuItem.Text = "Información";
+            this.informaciónToolStripMenuItem.Click += new System.EventHandler(this.informaciónToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("salirToolStripMenuItem.Image")));
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            this.opcionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ayudaToolStripMenuItem});
+            this.opcionesToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("opcionesToolStripMenuItem.Image")));
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            this.opcionesToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.opcionesToolStripMenuItem.Text = "Opciones";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ayudaToolStripMenuItem.Image")));
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            this.ayudaToolStripMenuItem.Click += new System.EventHandler(this.ayudaToolStripMenuItem_Click);
+            // 
+            // vistaToolStripMenuItem
+            // 
+            this.vistaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actualizarToolStripMenuItem});
+            this.vistaToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("vistaToolStripMenuItem.Image")));
+            this.vistaToolStripMenuItem.Name = "vistaToolStripMenuItem";
+            this.vistaToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.vistaToolStripMenuItem.Text = "Vista";
+            // 
+            // actualizarToolStripMenuItem
+            // 
+            this.actualizarToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("actualizarToolStripMenuItem.Image")));
+            this.actualizarToolStripMenuItem.Name = "actualizarToolStripMenuItem";
+            this.actualizarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.actualizarToolStripMenuItem.Text = "Actualizar";
+            this.actualizarToolStripMenuItem.Click += new System.EventHandler(this.actualizarToolStripMenuItem_Click);
+            // 
+            // tcProcesos
+            // 
+            this.tcProcesos.Controls.Add(this.tpProcesos);
+            this.tcProcesos.Controls.Add(this.tpRendimiento);
+            this.tcProcesos.Location = new System.Drawing.Point(12, 27);
+            this.tcProcesos.Name = "tcProcesos";
+            this.tcProcesos.SelectedIndex = 0;
+            this.tcProcesos.Size = new System.Drawing.Size(1046, 492);
+            this.tcProcesos.TabIndex = 1;
+            // 
+            // tpProcesos
+            // 
+            this.tpProcesos.Controls.Add(this.dgvProcesos);
+            this.tpProcesos.Location = new System.Drawing.Point(4, 25);
+            this.tpProcesos.Name = "tpProcesos";
+            this.tpProcesos.Padding = new System.Windows.Forms.Padding(3);
+            this.tpProcesos.Size = new System.Drawing.Size(1038, 463);
+            this.tpProcesos.TabIndex = 0;
+            this.tpProcesos.Text = "Procesos";
+            this.tpProcesos.UseVisualStyleBackColor = true;
+            // 
+            // dgvProcesos
+            // 
+            this.dgvProcesos.AllowUserToAddRows = false;
+            this.dgvProcesos.AllowUserToOrderColumns = true;
+            this.dgvProcesos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProcesos.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvProcesos.BackgroundColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProcesos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.dgvProcesos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvProcesos.DefaultCellStyle = dataGridViewCellStyle8;
+            this.dgvProcesos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvProcesos.Location = new System.Drawing.Point(3, 3);
+            this.dgvProcesos.MultiSelect = false;
+            this.dgvProcesos.Name = "dgvProcesos";
+            this.dgvProcesos.ReadOnly = true;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProcesos.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            this.dgvProcesos.RowHeadersWidth = 25;
+            this.dgvProcesos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProcesos.Size = new System.Drawing.Size(1032, 457);
+            this.dgvProcesos.TabIndex = 66;
+            // 
+            // tpRendimiento
+            // 
+            this.tpRendimiento.Location = new System.Drawing.Point(4, 25);
+            this.tpRendimiento.Name = "tpRendimiento";
+            this.tpRendimiento.Padding = new System.Windows.Forms.Padding(3);
+            this.tpRendimiento.Size = new System.Drawing.Size(1038, 463);
+            this.tpRendimiento.TabIndex = 1;
+            this.tpRendimiento.Text = "Rendimiento";
+            this.tpRendimiento.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnActualizar.Image = ((System.Drawing.Image)(resources.GetObject("btnActualizar.Image")));
+            this.btnActualizar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnActualizar.Location = new System.Drawing.Point(463, 525);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(180, 39);
+            this.btnActualizar.TabIndex = 5;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnFinalizarProceso
+            // 
+            this.btnFinalizarProceso.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinalizarProceso.ForeColor = System.Drawing.Color.Red;
+            this.btnFinalizarProceso.Image = ((System.Drawing.Image)(resources.GetObject("btnFinalizarProceso.Image")));
+            this.btnFinalizarProceso.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFinalizarProceso.Location = new System.Drawing.Point(874, 525);
+            this.btnFinalizarProceso.Name = "btnFinalizarProceso";
+            this.btnFinalizarProceso.Size = new System.Drawing.Size(180, 39);
+            this.btnFinalizarProceso.TabIndex = 67;
+            this.btnFinalizarProceso.Text = "Finalizar";
+            this.btnFinalizarProceso.UseVisualStyleBackColor = true;
+            this.btnFinalizarProceso.Click += new System.EventHandler(this.btnFinalizarProceso_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lblTotal.Location = new System.Drawing.Point(15, 535);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(155, 19);
+            this.lblTotal.TabIndex = 68;
+            this.lblTotal.Text = "Total de procesos: ";
+            // 
+            // txtPID
+            // 
+            this.txtPID.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPID.Location = new System.Drawing.Point(765, 532);
+            this.txtPID.Name = "txtPID";
+            this.txtPID.Size = new System.Drawing.Size(87, 26);
+            this.txtPID.TabIndex = 69;
+            this.txtPID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPID_KeyPress);
+            // 
+            // lblPID
+            // 
+            this.lblPID.AutoSize = true;
+            this.lblPID.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPID.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lblPID.Location = new System.Drawing.Point(666, 535);
+            this.lblPID.Name = "lblPID";
+            this.lblPID.Size = new System.Drawing.Size(93, 18);
+            this.lblPID.TabIndex = 70;
+            this.lblPID.Text = "Ingrese PID:";
+            // 
+            // Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1070, 576);
+            this.Controls.Add(this.lblPID);
+            this.Controls.Add(this.txtPID);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnFinalizarProceso);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.tcProcesos);
+            this.Controls.Add(this.msPrincipal);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.msPrincipal;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Admin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Administrador de tareas";
+            this.Load += new System.EventHandler(this.Admin_Load);
+            this.msPrincipal.ResumeLayout(false);
+            this.msPrincipal.PerformLayout();
+            this.tcProcesos.ResumeLayout(false);
+            this.tpProcesos.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProcesos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vistaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actualizarToolStripMenuItem;
+        private System.Windows.Forms.TabControl tcProcesos;
+        private System.Windows.Forms.TabPage tpProcesos;
+        private System.Windows.Forms.TabPage tpRendimiento;
+        private System.Windows.Forms.DataGridView dgvProcesos;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnFinalizarProceso;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtPID;
+        private System.Windows.Forms.Label lblPID;
+    }
+}
+
